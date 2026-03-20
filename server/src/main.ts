6aa7bd1553b662ff74c037905fb6a41bcb744451
Vladyslav317 <vladyslav3177@gmail.com>
2026-03-21 01:57:10 +0200

    fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-import serverlessExpress from '@vendia/serverless-express';
+import express, { Request, Response } from 'express';
 
 const expressApp = express();
-let cachedHandler: ReturnType<typeof serverlessExpress>;
 
-async function bootstrap() {
+async function createNestServer() {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressApp),
@@ -16,7 +14,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: 'https://portfolio-five-mu-bhmnyupuju.vercel.app',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
@@ -30,16 +28,27 @@ async function bootstrap() {
   );
 
   await app.init();
-
-  cachedHandler = serverlessExpress({ app: expressApp });
 }
 
-bootstrap();
+let bootstrapPromise = createNestServer();
 
-export const handler = async (req: any, res: any) => {
-  if (!cachedHandler) {
-    await bootstrap();
+export default async function handler(req: Request, res: Response) {
+  await bootstrapPromise;
+
+  if (req.method === 'OPTIONS') {
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://portfolio-five-mu-bhmnyupuju.vercel.app',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.status(204).send('');
+
+    return;
   }
 
-  return cachedHandler(req, res);
-};
+  expressApp(req, res);
+}
