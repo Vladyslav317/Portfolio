88503b881e3c1dc94fa68549d108335006f55039
Vladyslav317 <vladyslav3177@gmail.com>
2026-03-20 23:56:47 +0200

    setup vercel

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.setGlobalPrefix('api');
   app.enableCors();
@@ -14,7 +18,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT || 3000);
+
+  await app.init();
 }
 
 bootstrap();
+
+export default server;
