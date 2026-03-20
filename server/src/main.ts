55283cafb9ca5898ed2c50124e95c47be471930a
Vladyslav317 <vladyslav3177@gmail.com>
2026-03-21 00:34:34 +0200

    fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
+import serverlessExpress from '@vendia/serverless-express';
 
-const server = express();
+const expressApp = express();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+async function createNestServer() {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
 
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://portfolio-five-mu-bhmnyupuju.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
@@ -26,8 +30,12 @@ async function bootstrap() {
   );
 
   await app.init();
+
+  return expressApp;
 }
 
-bootstrap();
+export const handler = async (req: any, res: any) => {
+  const app = await createNestServer();
 
-export default server;
+  return serverlessExpress({ app })(req, res);
+};
